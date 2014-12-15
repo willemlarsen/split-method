d83cf42d46e796b0cd1f22dbdbaa1e8fc2831ed9
Willem Larsen <willemlarsen@gmail.com>
2014-12-15 13:01:33 -0800

    Second commit

--- lib/split_method.rb
+++ lib/split_method.rb
@@ -1,10 +1,27 @@
-def splitter(words)
-  words_array = []
-  index = words.length
+def character_splitter(string)
+  string_array = []
+  index = string.length
   word_index = 0
   index.times do 
-    words_array.push(words[word_index])
+    string_array.push(string[word_index])
     word_index += 1
   end
-  words_array
+  string_array
+end
+
+def return_first_word(string_array)
+  first_word = ""
+  word_array = []
+  string_array.each do |character|
+    word_array.push(first_word) if character == " "
+    first_word += character 
+  end
+  word_array
+end
+
+def word_splitter(phrase)
+  word_array = []
+  split_letters = character_splitter(phrase)
+  first_word = return_first_word(split_letters)
+  word_array.push(first_word)
 end
--- spec/split_method_spec.rb
+++ spec/split_method_spec.rb
@@ -1,11 +1,10 @@
 require 'split_method'
 
 describe 'splitter' do 
-  it 'converts a string into an list of words' do 
+  it 'converts a string into a list of words' do 
     string_a = 'Go there'
     string_b = 'Come here'
-    expect(splitter(string_a)).to eq(['Go', 'there'])
-    expect(splitter(string_b)).to eq(['Come', 'here'])
-
+    expect(character_splitter(string_a)).to eq(['Go', 'there'])
+    expect(character_splitter(string_b)).to eq(['Come', 'here'])
   end
 end
