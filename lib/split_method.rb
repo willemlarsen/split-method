476ed9454fada06cccd32d53fc3fd11f1f0d4fcd
Willem Larsen <willemlarsen@gmail.com>
2014-12-15 15:56:55 -0800

    Add first_word! method

--- lib/split_method.rb
+++ lib/split_method.rb
@@ -9,19 +9,36 @@ def character_splitter(string)
   string_array
 end
 
-def return_first_word(string_array)
-  first_word = ""
+def return_words(string, character = " ")
+  split_character = character
+  string_array = character_splitter(string)
+  current_word = ""
   word_array = []
-  string_array.each do |character|
-    word_array.push(first_word) if character == " "
-    first_word += character 
+  string_array.each do |character|  
+    if character == split_character 
+      word_array_push(current_word, word_array) 
+      current_word = ""             
+    else current_word += character  
+    end
   end
+  word_array_push(current_word, word_array)
   word_array
 end
 
-def word_splitter(phrase)
-  word_array = []
-  split_letters = character_splitter(phrase)
-  first_word = return_first_word(split_letters)
-  word_array.push(first_word)
+def word_array_push(word, array)
+  array.push(word) unless word == ""
 end
+
+def first_word!(chars)
+  current_char = nil
+  word = ""
+  while current_char != " "
+    current_char = chars.shift
+    if current_char != " "
+      word += current_char
+    else return word
+    end
+  end
+end
+
+
--- spec/split_method_spec.rb
+++ spec/split_method_spec.rb
@@ -2,9 +2,28 @@ require 'split_method'
 
 describe 'splitter' do 
   it 'converts a string into a list of words' do 
-    string_a = 'Go there'
+    string_a = '   Go  there     '
     string_b = 'Come here'
-    expect(character_splitter(string_a)).to eq(['Go', 'there'])
-    expect(character_splitter(string_b)).to eq(['Come', 'here'])
+    expect(return_words(string_a)).to eq(['Go', 'there'])
+    expect(return_words(string_b)).to eq(['Come', 'here'])
+  end
+
+  it 'can use any character as a separator' do 
+    string_a = 'Go there'
+    expect(return_words(string_a, "e")).to eq(['Go th', 'r'])
   end
 end
+
+describe 'first_word!' do 
+  it 'consumes the first word in a list of characters' do 
+    list = ['G', 'o', ' ', 't', 'h', 'e', 'r', 'e']
+    expect(first_word!(list)).to eq('Go')
+  end
+  it 'removes the first word from the original list' do 
+    list = ['G', 'o', ' ', 't', 'h', 'e', 'r', 'e']
+    first_word!(list)
+    expect(list).to eq(['t', 'h', 'e', 'r', 'e'])
+  end
+end
+
+
